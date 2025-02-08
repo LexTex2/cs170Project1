7765586f380d75d631edf8bba08364bf9ee56ac2
Alexis <astep021@ucr.edu>
2025-02-08 00:58:43 -0800

    algoIt class rough planning

--- iteration.cpp
+++ iteration.cpp
@@ -1,2 +1,15 @@
 #include "iteration.h"
 
+void expand(const node &searchNode)   {
+
+}  
+    
+
+void explore()   {
+
+}
+    
+
+bool repeatedNode(const node &searchNode)   {
+
+}
--- iteration.h
+++ iteration.h
@@ -2,28 +2,24 @@
 #define ITERATION_H
 #include <cmath>
 #include <iostream>
+#include <stack>
 #include <queue>
 
 using namespace std;
 
 struct node   {
-    int puzzle[3][3];
-    int row;
-    int col;
-    int heuristic;
+    int puzzle[3][3];   //2D matrix array
+    int row;    //rows (j)
+    int col;    //columns (i)
+    int heuristic;  //h(n)
     int depth;
     
     bool operator==(const node nextVal) const;  //used to compare if final and current puzzle matrix are equiv
-
     bool operator<(const node nextVal) const;   
-    node &operator=(const node nextVal);
-
-    //node::node();
-      
+    node &operator=(const node nextVal);      
 };
 
 bool node::operator==(const node nextVal) const {
-
     for(int j = 0; j < 3; j++)   {     //j = row #
         for(int i = 0; i < 3; i++)   {     //i = col #
             if (nextVal.puzzle[j][i] != puzzle[j][i])   {     //compare equiv positions value between current puzzle and puzzle soltion
@@ -55,14 +51,25 @@ node &node::operator=(const node nextVal)  {
     return *this;
 }
 
-// node::node()   {
-//     for(int j = 0; j < 3; j++)   {     //j = row #
-//         for(int i = 0; i < 3; i++)   {     //i = col #
-//             puzzle[j][i] = 0;
-//         }
-//     }
-// }
+class algoIteration   {
+    public:
+        node final;     //soltuion matrix
+        int algoPicked;     //for later
+        node orig;      //passed in/starter matrix
+        int nodesTotal;
+        priority_queue<node> rawPush;   //priority queue
+        vector<node> oldNodes;      //vector for nodes already visited/to not repeat
 
+    void expand(const node &searchNode);  //
+    void explore();
+    bool repeatedNode(const node &searchNode);
+
+
+
+
+
+
+}
 
 
 #endif
